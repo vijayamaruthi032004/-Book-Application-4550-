f5f88a769b6a29c3d37465e8aba3b3816c733c6b
vijayamaruthi032004 <vijayamaruthi10@gmail.com>
2025-08-02 10:15:35 +0530

    Create Contact.java

---
+++ Contact.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Contact {
+    private String name;
+    private String email;
+
+    public Contact(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+
+    public void setName(String name) { this.name = name; }
+    public void setEmail(String email) { this.email = email; }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Email: " + email;
+    }
+}
